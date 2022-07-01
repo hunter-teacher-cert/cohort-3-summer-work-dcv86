6420f7c7f467deb307c590ab21db2cf7d50bb5fa
DCV86 <97967740+dcv86@users.noreply.github.com>
2022-07-01 17:35:14 +0000

    arrayPractice upload

---
+++ programming/1/Nim.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim
+	{
+		public static void main(String[] args)
+		{
+			int stones = 12;
+			int stonesTaken;
+
+			Scanner input = new Scanner(System.in);
+
+			//loop until game ends
+
+			while (){
+0
+			}
+		}
+		
+	}
